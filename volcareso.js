8357f07831e7e8b3a5dbee930f70b04591f0fa75
volcareso <89912144+volcareso@users.noreply.github.com>
2021-09-01 16:29:33 +0530

    Renaming

--- volcareso.js
+++ volcareso.js
@@ -1,18 +1,3 @@
-//-------------------------------------------
-//-------------------------------------------
-
-// (C) SILICONDUCT 2021
-// APACHE LICENCE 2.0
-
-// HASHTAG_1.0.1.JS (UNCOMPRESSED VERSION)
-// SIZE : 6KB APPROX
-// CONTRIBUTORS:
-//       -> RAGHAV9OFFICIAL
-
-
-//-------------------------------------------
-//-------------------------------------------
-
 "use strict";
 const hash = (sel, option) => {
 
